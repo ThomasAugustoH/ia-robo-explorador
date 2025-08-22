aba35232eac57010ea8f148d60baa5063d18687f
ThomasAugustoH <thomas.a.haskel@gmail.com>
2025-08-21 23:11:45 -0300

    Colorindo o caminho

--- Etapa3/main.py
+++ Etapa3/main.py
@@ -1,53 +1,62 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
-
-
 def main():
     grid = nx.grid_2d_graph(10, 10)
-    list(nx.connected_components(grid))
-    pos = {node: node for node in grid.nodes()}
-
+   
     remove_nodes(grid, 1)
+    path = create_path(grid, (0,9), (7,2))
+    print_graph(grid, path)
 
-    path = find_path(grid, (0,9), (7,2))
-    unaccessed_nodes = list(grid.nodes)
+def create_path(graph, source, destination) -> list:
+    path = find_path(graph, source, destination)
+    colored_path = color_path(graph, path)
+    return colored_path
 
-    print(unaccessed_nodes)
-
-    nx.draw_networkx_nodes(grid, pos=pos, nodelist=path, node_color="tab:red")
-
-    nx.draw(grid, pos=pos, with_labels=True, font_weight='bold')
-    plt.show()
-
-def find_path(graph, source, destination) :
+def find_path(graph, source, destination) -> list:
     path = nx.dijkstra_path(graph, source, destination)
     return path
 
-def remove_nodes(grid, map):
+def color_path(graph, path) -> list:
+    color_map = []
+    for node in graph:
+        if node in path:
+            color_map.append('#F08C01')
+        else:
+            color_map.append('#DDDDDD')
+    return color_map
+
+def print_graph(graph, color_map):
+    pos = {node: node for node in graph.nodes()}
+    nx.draw_networkx(graph, pos=pos, node_color=color_map, with_labels=False)
+    plt.tight_layout()
+    plt.axis("off")
+    plt.show()
+
+def remove_nodes(graph, map):
     match map:
         case 1:
-            grid.remove_node((5, 0))
-            grid.remove_node((5, 1))
-            grid.remove_node((7, 1))
-            grid.remove_node((8, 1))
-            grid.remove_node((5, 2))
-            grid.remove_node((8, 2))
-            grid.remove_node((5, 3))
-            grid.remove_node((8, 3))
-            grid.remove_node((3, 4))
-            grid.remove_node((5, 4))
-            grid.remove_node((6, 4))
-            grid.remove_node((7, 4))
-            grid.remove_node((8, 4))
-            grid.remove_node((1, 5))
-            grid.remove_node((6, 5))
-            grid.remove_node((2, 6))
-            grid.remove_node((5, 6))
-            grid.remove_node((2, 7))
-            grid.remove_node((0, 8))
-            grid.remove_node((3, 8))
-            grid.remove_node((4, 9))
+            graph.remove_node((5, 0))
+            graph.remove_node((5, 1))
+            graph.remove_node((7, 1))
+            graph.remove_node((8, 1))
+            graph.remove_node((5, 2))
+            graph.remove_node((8, 2))
+            graph.remove_node((5, 3))
+            graph.remove_node((8, 3))
+            graph.remove_node((3, 4))
+            graph.remove_node((5, 4))
+            graph.remove_node((6, 4))
+            graph.remove_node((7, 4))
+            graph.remove_node((8, 4))
+            graph.remove_node((1, 5))
+            graph.remove_node((6, 5))
+            graph.remove_node((2, 6))
+            graph.remove_node((5, 6))
+            graph.remove_node((2, 7))
+            graph.remove_node((0, 8))
+            graph.remove_node((3, 8))
+            graph.remove_node((4, 9))
 
 if __name__ == "__main__":
     main()
