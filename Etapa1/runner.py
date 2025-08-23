07751ed5d182f8154cd771cb32dd0e3cdf0757fe
Caio Abraão Manarim <caioabraao10@gmail.com>
2025-08-23 15:21:44 -0300

    random start

--- Etapa1/runner.py
+++ Etapa1/runner.py
@@ -1,10 +1,16 @@
+import random as rnd
 from robot import Robot
 
 
 rows, coluns = 10, 10
 grid = [[0] * coluns for _ in range(rows)]
 
-robot = Robot(grid, 0, 0)
+start_x = rnd.randint(0, coluns - 1)
+start_y = rnd.randint(0, rows - 1)
+
+print(f"Robô iniciando na posição aleatória: ({start_x}, {start_y})")
+
+robot = Robot(grid, start_x, start_y)
 
 while robot.direction < 5:
     while robot.can_move_forward():
